a1c636941d7343e02b2c9ebd4ecc559301c5e4a8
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-12-07 14:30:37 +0200

    customized admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email") # it allows search using any of the parameters
+    list_filter = ("date", "occupation") # it filters by date or occupation
+    ordering = ("first_name", )  # it sort it in alphabetical order
+    readonly_fields = ("occupation", ) # this prevent the admin from modifying any of perimeters mentioned
+
+
+admin.site.register(Form, FormAdmin)
